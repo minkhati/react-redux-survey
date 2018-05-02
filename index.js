aa9263ddc81c541c849507dd7fd4ebdf4084775e
Min Khati <meetup.min@gmail.com>
2018-05-02 15:14:50 -0400

    setup mongodb in mlab (create mongodb database, database user), connect mongoose to mongo

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport'); // just caall passport file from services folder
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app); // authRoutes will call app as a parameter
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT);
