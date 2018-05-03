ce78fefdf4e643b4f41ba550fe9495ea27dab238
Min Khati <meetup.min@gmail.com>
2018-05-03 10:18:39 -0400

    Define /api/current_user and /api/logout routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,13 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // req.logout() function is automatically attached with req by passport
+    res.send(req.user); // Send user is looged out so undefined will be returned
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
