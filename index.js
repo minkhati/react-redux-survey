bd1b1fe0a782ad3ca71e95bec057a862d55b2b21
Min Khati <meetup.min@gmail.com>
2018-05-06 17:39:45 -0400

    define billingRoutes, install bodyParser for parsing incoming request for expresss server, install stripe for creating a charge object, finalize charge

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 require('./models/User'); // this should load before passport
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json()); // any post request comes in get parsed into json
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 10000, // in milliseconds 30 days
@@ -22,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app); // authRoutes will call app as a parameter
+require('./routes/billingRoutes')(app); //
 
 const PORT = process.env.PORT || 5000;
 
