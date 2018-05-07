48fffe71a466b454240b513aa6605e54a38a8edc
Min Khati <meetup.min@gmail.com>
2018-05-07 15:07:41 -0400

    create requireCredits middleware and verify credits before creating any survey

--- routes/serveyRoutes.js
+++ routes/serveyRoutes.js
@@ -1,7 +1,8 @@
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middleware/requireCredits');
 
 module.exports = app => {
   // Before creating a survey user should be loggedin and should have sufficient credits
   // when someone send post request '/api/surveys' then make sure requireLogin runs
-  app.post('/api/surveys', requireLogin, (req, res) => {});
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
 };
