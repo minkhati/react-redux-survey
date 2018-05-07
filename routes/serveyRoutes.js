397ccfa3119d3bef211c18eff344b1fda88da927
Min Khati <meetup.min@gmail.com>
2018-05-07 18:44:34 -0400

    create survey document and recipients sub document

--- routes/serveyRoutes.js
+++ routes/serveyRoutes.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middleware/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   // Before creating a survey user should be loggedin and should have sufficient credits
   // when someone send post request '/api/surveys' then make sure requireLogin runs
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
